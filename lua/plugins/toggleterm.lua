798acebba3f2a18dfa80ebfffa5ec88cba7915ca
Bernard Cooke <bernard-cooke@hotmail.com>
2026-06-22 12:14:19 +0100

    feat: update some keymaps for toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,72 +1,94 @@
+local vsplit_win_size = vim.api.nvim_win_get_width(0) / 3
+local hsplit_win_size = vim.api.nvim_win_get_height(0) / 3
+
 return {
-	"akinsho/toggleterm.nvim",
-	opts = {--[[ things you want to change go here]]
-	},
-	config = true,
-	keys = {
-	    {
-		"<leader>tt",
-		function()
-		    vim.cmd.TermSelect({})
-		end,
-		desc = "Select [T]erminal",
-	    },
-	    {
-		"<leader>ta",
-		function()
-		    vim.cmd.ToggleTermToggleAll({})
-		end,
-		desc = "Toggle [A]ll Terminals",
-	    },
-	    {
-		"<leader>th",
-		function()
-		    vim.cmd.ToggleTerm({
-			args = {
-			    string.format("size=%d", vsplit_win_size),
-			    'direction="vertical"',
-			    string.format("dir=%s", vim.fn.getcwd()),
-			},
-		    })
-		end,
-		desc = "Toggle [H]orizontal Terminal",
-	    },
-	    {
-		"<leader>tp",
-		function()
-		    local python3 = vim.fn.executable("python3") == 1 and "python3"
-		    if python3 then
-			return vim.cmd.TermExec({
-			    args = {
-				"cmd=python3",
-				string.format("size=%d", vsplit_win_size),
-				"name=python3",
-				'direction="vertical"',
-				string.format("dir=%s", vim.fn.getcwd()),
-			    },
-			})
-			-- TODO: this would set insert mode, but we are still in the wrong buffer
-			-- local keys = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
-			-- vim.api.nvim_feedkeys(keys, "i", true)
-		    end
-		    local python = vim.fn.executable("python") == 1 and "python"
-		    if python then
-			vim.cmd.TermExec({
-			    args = {
-				"cmd=python",
-				string.format("size=%d", vsplit_win_size),
-				"name=python",
-				'direction="vertical"',
-				string.format("dir=%s", vim.fn.getcwd()),
-			    },
-			})
-			-- TODO: this would set insert mode, but we are still in the wrong buffer
-			-- local keys = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
-			-- vim.api.nvim_feedkeys(keys, "i", true)
-		    end
-		end,
-		desc = "ToggleTerm python",
-	    },
-	},
-	lazy = false,
+  "akinsho/toggleterm.nvim",
+  opts = {--[[ things you want to change go here]]
+  },
+  config = true,
+  keys = {
+    {
+      "<leader>tt",
+      function()
+        vim.cmd.TermSelect({})
+      end,
+      desc = "Select [T]erminal",
+    },
+    {
+      "<leader>ta",
+      function()
+        vim.cmd.ToggleTermToggleAll({})
+      end,
+      desc = "Toggle [A]ll Terminals",
+    },
+    {
+      "<leader>th",
+      function()
+        vim.cmd.ToggleTerm({
+          args = {
+            string.format("size=%d", hsplit_win_size),
+            'direction="horizontal"',
+            string.format("dir=%s", vim.fn.getcwd()),
+          },
+        })
+      end,
+      desc = "Toggle [H]orizontal Terminal",
+    },
+    {
+      "<leader>tp",
+      function()
+        local python3 = vim.fn.executable("python3") == 1 and "python3"
+        local python = vim.fn.executable("python") == 1 and "python"
+        local command = python3 or python or nil
+        if command then
+          return require("toggleterm").exec(
+            command,
+            0,
+            hsplit_win_size,
+            vim.uv.cwd(),
+            "horizontal",
+            "Python 3",
+            false,
+            true
+          )
+        end
+      end,
+      desc = "ToggleTerm python",
+    },
+    {
+      "<leader>rp",
+      function()
+        local python3 = vim.fn.executable("python3") == 1 and "python3"
+        local python = vim.fn.executable("python") == 1 and "python"
+        local command = python3 or python or nil
+        if command then
+          return require("toggleterm").exec(
+            string.format("%s %s", command, vim.fn.expand("%")),
+            0,
+            hsplit_win_size,
+            vim.uv.cwd(),
+            "horizontal",
+            "Python 3",
+            false,
+            true
+          )
+        end
+      end,
+      desc = "Run python file",
+    },
+    {
+      "<leader>tv",
+      function()
+        vim.cmd.ToggleTerm({
+          args = {
+            string.format("size=%d", vsplit_win_size),
+            'direction="vertical"',
+            string.format("dir=%s", vim.uv.cwd()),
+          },
+        })
+      end,
+      desc = "Toggle [V]ertical Terminal",
+    },
+  },
+  lazy = false,
 }
