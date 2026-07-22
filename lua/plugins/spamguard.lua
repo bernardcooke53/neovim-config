a45db7dc21d7b95b16c03b8185bc67d10c4a932c
Bernard Cooke <bernard-cooke@hotmail.com>
2026-07-22 16:31:05 +0100

    more plugins

---
+++ lua/plugins/spamguard.lua
@@ -0,0 +1,20 @@
+return {
+  "timseriakov/spamguard.nvim",
+  event = "VeryLazy",
+  config = function()
+    local spamguard = require("spamguard")
+    spamguard.setup({
+      keys = {
+        j = { threshold = 9, suggestion = "use s / f instead of spamming jjjj" },
+        k = { threshold = 9, suggestion = "try { of ( instead of spamming kkkk" },
+        h = { threshold = 9, suggestion = "use b / 0 / ^ instead of spamming hhhh" },
+        l = { threshold = 9, suggestion = "try w / e / f — it's faster!" },
+        w = { threshold = 8, suggestion = "use s / f — more precise and quicker!" },
+        b = { threshold = 8, suggestion = "use s / F — more precise and quicker!" },
+      },
+    })
+    vim.schedule(function()
+      spamguard.enable()
+    end)
+  end,
+}
